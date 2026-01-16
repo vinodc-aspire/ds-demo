cf81133bda0abff196835b7e3a353f7e0f782033
Vinod C <viinodc@gmail.com>
2026-01-16 13:13:32 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy/ds/:path*",
+        destination: "https://api-dev.landau.app/v1/ds/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
